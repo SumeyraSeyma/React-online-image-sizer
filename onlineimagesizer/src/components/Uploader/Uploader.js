96a3f6e10276bf6e89225e3060d85177abd10f9e
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-08-21 13:08:40 +0300

    the picture was drawn on canvas

--- onlineimagesizer/src/components/Uploader/Uploader.js
+++ onlineimagesizer/src/components/Uploader/Uploader.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import './Uploader.css'
 import { MdCloudUpload,MdDelete } from 'react-icons/md'
 import {AiFillFileImage} from 'react-icons/ai'
@@ -6,6 +6,26 @@ import {AiFillFileImage} from 'react-icons/ai'
 function Uploader() {
     const [image,setImage] = useState(null)
     const [fileName,setFileName] = useState('No selected file')
+    const canvasRef = useRef(null); // Canvas referansı oluşturuldu
+
+    // Resim yüklendiğinde canvas'a çizme işlemini gerçekleştiren useEffect
+    useEffect(() => {
+        if (image) {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext('2d');
+            const img = new Image();
+            img.src = image;
+            img.onload = () => {
+                // Canvas boyutlarını ayarla
+                canvas.width = img.width;
+                canvas.height = img.height;
+                // Resmi canvas'a çiz
+                ctx.drawImage(img, 0, 0);
+                // Canvas'a çizimin gerçekleştiğini kontrol et
+                console.log("Resim canvas üzerine çizildi.");
+            };
+        }
+    }, [image]);
 
   return (
     <main>
@@ -25,13 +45,7 @@ function Uploader() {
         />
         
         {image ?(
-        <img 
-        src={image} 
-        style={{ maxWidth: "100%", 
-            maxHeight: "100%", 
-            objectFit: "contain" }}  
-            alt={fileName}
-            />
+        <canvas ref={canvasRef} style={{ maxWidth: '100%', maxHeight: '100%' }} />
         ):(
         <>
         <MdCloudUpload color='#1475cf' size={60}/>
@@ -49,7 +63,11 @@ function Uploader() {
                 size={30}
                 onClick={()=>{
                     setImage(null)
-                    setFileName('No selected file')
+                    setFileName('No selected file');
+                    // Canvas'ı temizlemek için
+                    const canvas = canvasRef.current;
+                    const ctx = canvas.getContext('2d');
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
                 }}
                 />
             </span>
